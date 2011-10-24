bda15e95fd23c1874388d7528762f6649ac40557
Matt MacEwan(GLaDOS) <mrm4677@rit.edu>
2011-10-24 14:06:43 -0400

    Initial implementation of solutions
    List monad ahoy!

--- Puzzle.hs
+++ Puzzle.hs
@@ -2,6 +2,8 @@
 module Puzzle
 where
 
+import Control.Monad (mplus)
+
 -- | @Puzzle@ is a class of puzzles solved by brute-force backtracking.
 -- The class requires 'solved' and 'choices'
 -- and provides 'solutions' and 'solve'.
@@ -12,7 +14,13 @@ class Eq p => Puzzle p where
   -- puzzle is not solved.
   choices :: p -> [p]
 
-  -- | @solve puzzle@ returns the first solution of a puzzle, if any.
-  solve :: p -> p
   -- | @solutions puzzle@ returns a list of all solutions for a puzzle.
   solutions :: p -> [p]
+  solutions p = do
+    choice <- choices p
+    (do True <- [solved choice]
+        return choice)
+      `mplus` (solutions choice)
+  -- | @solve puzzle@ returns the first solution of a puzzle, if any.
+  solve :: p -> p
+  solve p = head (solutions p)
